b65592030acf10271f589ffc7e388f83e623417c
cxalem <smarketing.cuz@gmail.com>
2025-08-19 14:23:11 +0200

    fix: improve navbar layout for better responsiveness
    - Adjusted padding and alignment in the navbar for improved mobile and desktop views.
    - Enhanced the layout to ensure a more balanced appearance and better user experience across devices.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,8 +10,8 @@ interface NavbarProps {
 
 export function Navbar({ user, isAdmin }: NavbarProps) {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
+    <header className="sticky px-1 md:px-4 top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex justify-between h-14 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-4 flex items-center space-x-2 cursor-pointer">
             <span className="text-xl font-bold">⚽</span>
@@ -21,8 +21,8 @@ export function Navbar({ user, isAdmin }: NavbarProps) {
           </Link>
         </div>
         
-        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-          <nav className="flex items-center space-x-2">
+        <div className="flex flex-1 items-end justify-end space-x-2 md:justify-end">
+          <nav className="flex items-center space-x-2 ">
             <ThemeToggle />
             <AuthButton user={user} isAdmin={isAdmin} />
           </nav>
